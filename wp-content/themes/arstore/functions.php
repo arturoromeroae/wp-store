f3bfd39826e0b6bd273395b561b6dbb7f881a0dd
Arturo Romero <jromero@csticorp.biz>
2025-07-27 22:16:21 -0500

    se modifica el tema para usar elementor y woocommerce

--- wp-content/themes/arstore/functions.php
+++ wp-content/themes/arstore/functions.php
@@ -9,3 +9,15 @@ add_action( 'after_setup_theme', 'mi_tema_setup' );
 
 // Agregar soporte para el título dinámico
 add_theme_support( 'title-tag' );
+
+// Soporte para WooCommerce
+function mi_tema_woocommerce_setup() {
+    add_theme_support('woocommerce');
+}
+add_action('after_setup_theme', 'mi_tema_woocommerce_setup');
+
+// Declarar compatibilidad con Elementor
+function mi_tema_elementor_setup() {
+    add_theme_support('elementor');
+}
+add_action('after_setup_theme', 'mi_tema_elementor_setup');
